3ba0db314ede62aa2f6fe15a27b2c9d9a9a5ca42
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2022-12-16 05:31:33 +0530

    add

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -16,7 +16,7 @@
 
     <div class="row my-2 justify-content-end">
         <div class="mx-3">
-            <a href="{{ route('categories.create') }}" class="btn btn-primary btn-sm">New Category</a>
+            <a href="{{ route('categories.create') }}"  data-toggle="modal" data-target="#new-category-modal" class="btn btn-primary btn-sm">New Category</a>
         </div>
     </div>
     <div class="row">
@@ -45,6 +45,37 @@
                     @endforelse
                 </tbody>
             </table>
+        </div>
+    </div>
+
+
+    <div class="modal fade" role="dialog" aria-modal="true" id="new-category-modal">
+        <div class="modal-dialog  modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header d-block text-center pb-3 border-bttom">
+                    <h3 class="modal-title" id="exampleModalCenterTitle01">New Line</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group mb-3">
+                                <form action="{{ route('lines.store') }}" method="post">
+                                    @csrf
+                                <label for="exampleInputText01" class="h5">Delivery Area*</label>
+                                <input id="name" name="name" type="text" class="form-control" placeholder="Delivery Area" />
+                            </div>
+                        </div>
+                 
+                        <div class="col-lg-12">
+                            <div class="d-flex flex-wrap align-items-ceter justify-content-center mt-2">
+                                <button type="submit" class="btn btn-primary mr-3">Save</button>
+                                <button class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
