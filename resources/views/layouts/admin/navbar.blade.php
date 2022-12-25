6860f9d862782bc6760b1551e8bda6727afdc63a
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2022-12-25 16:53:41 +0530

    app

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -24,115 +24,21 @@
                 <!-- Header Menu -->
                 <ul class="nav user-menu">
                 
-                    <!-- Search -->
-                    <li class="nav-item">
-                        <div class="top-nav-search">
-                            
-                            <a href="javascript:void(0);" class="responsive-search">
-                                <i class="fa fa-search"></i>
-                        </a>
-                            <form action="#">
-                                <div class="searchinputs">
-                                    <input type="text" placeholder="Search Here ...">
-                                    <div class="search-addon">
-                                        <span><img src="{{ asset('assets/img/icons/closes.svg') }}" alt="img"></span>
-                                    </div>
-                                </div>
-                                <a class="btn"  id="searchdiv"><img src="{{ asset('assets/img/icons/search.svg') }}" alt="img"></a>
-                            </form>
-                        </div>
-                    </li>
-                    <!-- /Search -->
+
                 
                     <!-- Flag -->
                    
                     <!-- /Flag -->
                 
-                    <!-- Notifications -->
+                    <!-- Shopping Cart -->
                     <li class="nav-item dropdown">
-                        <a href="javascript:void(0);" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                            <img src="{{ asset('assets/img/icons/notification-bing.svg') }}"   alt="img"> <span class="badge rounded-pill">4</span>
+                        <a href="javascript:void(0);" class="av-link">
+                            <i class="fa fa-shopping-cart" data-bs-toggle="tooltip" title="" data-bs-original-title="fa fa-shopping-cart" aria-label="fa fa-shopping-cart" aria-describedby="tooltip883272"></i>
+                             <span class="badge rounded-pill">4</span>
                         </a>
-                        <div class="dropdown-menu notifications">
-                            <div class="topnav-dropdown-header">
-                                <span class="notification-title">Notifications</span>
-                                <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-                            </div>
-                            <div class="noti-content">
-                                <ul class="notification-list">
-                                    <li class="notification-message">
-                                        <a href="activities.html">
-                                            <div class="media d-flex">
-                                                <span class="avatar flex-shrink-0">
-                                                    <img alt="" src="{{ asset('assets/img/profiles/avatar-02.jpg') }}">
-                                                </span>
-                                                <div class="media-body flex-grow-1">
-                                                    <p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
-                                                    <p class="noti-time"><span class="notification-time">4 mins ago</span></p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="notification-message">
-                                        <a href="activities.html">
-                                            <div class="media d-flex">
-                                                <span class="avatar flex-shrink-0">
-                                                    <img alt="" src="{{ asset('assets/img/profiles/avatar-03.jpg') }}">
-                                                </span>
-                                                <div class="media-body flex-grow-1">
-                                                    <p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
-                                                    <p class="noti-time"><span class="notification-time">6 mins ago</span></p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="notification-message">
-                                        <a href="activities.html">
-                                            <div class="media d-flex">
-                                                <span class="avatar flex-shrink-0">
-                                                    <img alt="" src="{{ asset('assets/img/profiles/avatar-06.jpg') }}">
-                                                </span>
-                                                <div class="media-body flex-grow-1">
-                                                    <p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-                                                    <p class="noti-time"><span class="notification-time">8 mins ago</span></p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="notification-message">
-                                        <a href="activities.html">
-                                            <div class="media d-flex">
-                                                <span class="avatar flex-shrink-0">
-                                                    <img alt="" src="{{ asset('assets/img/profiles/avatar-17.jpg') }}">
-                                                </span>
-                                                <div class="media-body flex-grow-1">
-                                                    <p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
-                                                    <p class="noti-time"><span class="notification-time">12 mins ago</span></p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="notification-message">
-                                        <a href="activities.html">
-                                            <div class="media d-flex">
-                                                <span class="avatar flex-shrink-0">
-                                                    <img alt="" src="{{ asset('assets/img/profiles/avatar-13.jpg') }}">
-                                                </span>
-                                                <div class="media-body flex-grow-1">
-                                                    <p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
-                                                    <p class="noti-time"><span class="notification-time">2 days ago</span></p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="topnav-dropdown-footer">
-                                <a href="activities.html">View all Notifications</a>
-                            </div>
-                        </div>
+                        
                     </li>
-                    <!-- /Notifications -->
+                    <!-- /Shopping Cart -->
                     
                     <li class="nav-item dropdown has-arrow main-drop">
                         <a href="javascript:void(0);" class="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
@@ -162,11 +68,16 @@
                 
                 <!-- Mobile Menu -->
                 <div class="dropdown mobile-user-menu">
+                 
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
                     <div class="dropdown-menu dropdown-menu-right">
+
+                        <a href="javascript:void(0);" class="dropdown-item"> <img src="http://localhost:8000/assets/img/icons/sales1.svg" alt=""> View Cart </a>
                         <a class="dropdown-item" href="">My Profile</a>
                         <a class="dropdown-item" href="">Settings</a>
                         <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+
+                        
                     </div>
                 </div>
                 <!-- /Mobile Menu -->
