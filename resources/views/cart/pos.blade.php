4f0ad83b61454db4758d3265514578e3b75c868f
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-01-08 20:03:40 +0530

    app

--- resources/views/cart/pos.blade.php
+++ resources/views/cart/pos.blade.php
@@ -18,7 +18,7 @@
                     border-width: none; 
                     box-shadow: none;
                     background: transparent;
-                    width: 84px; /*new width*/
+                    max-width: 200px; /*new width*/
                     height: 24px; /*new width*/
                 }         
                 .sminput.focus {
