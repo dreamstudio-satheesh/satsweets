b51330a8a0c48b7c3774e73e70932e4f9385dc52
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-01-29 13:35:32 +0530

    app

--- resources/views/livewire/pos.blade.php
+++ resources/views/livewire/pos.blade.php
@@ -45,7 +45,7 @@
 								</div>
 								<div class="productsetcontent">
 									<h5>{{$item->code}}</h5>
-									<p style="font-size: 12px;">>{{$item->name}}</p>
+									<strong><p style="font-size: 12px;">{{$item->name}}</p></strong>
 									<h6>{{$item->price}}</h6>
 								</div>
 							</div>
