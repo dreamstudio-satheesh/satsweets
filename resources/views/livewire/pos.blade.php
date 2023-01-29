f0ce4a98e088e3c47cef4665ce9ea6b6f6923a8d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-01-29 12:33:26 +0530

    app

--- resources/views/livewire/pos.blade.php
+++ resources/views/livewire/pos.blade.php
@@ -5,7 +5,7 @@
 				<div class="row">
                     @foreach ($categories as $category)
 
-                    <div class="col-lg-12" style="width: 98px; margin-right: 5px;" wire:click="category({{ $category->id }})">						
+                    <div class="col-lg-12" style="width: 98px; margin-right: 4px;" wire:click="category({{ $category->id }})">						
 						<div class="product-details ">
                                 @if ( !empty( $category->getFirstMediaUrl('categories') ) )                                          
                                 <img src="{{ $category->getFirstMediaUrl('categories', 'thumb') }}">                                        
