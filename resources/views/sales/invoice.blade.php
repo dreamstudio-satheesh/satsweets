26bbcc61855026300246d5c1085c7018e69cfb64
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-02-10 00:39:34 +0530

    app

--- resources/views/sales/invoice.blade.php
+++ resources/views/sales/invoice.blade.php
@@ -121,7 +121,7 @@
     <p align="right" class="text-2">For SATSWEETS</p>
   </div>
   <footer class="text-center mt-4">
-    <br><br><br>
+    <br>
   <p class="text-1"><strong>NOTE :</strong> This is computer generated receipt and does not require physical signature.</p>
 
   <div class="btn-group btn-group-sm d-print-none"><button onclick="window.print()" class="btn btn-light border text-black-50 shadow-none"><i class="fa fa-print"></i> Print</button> <a href="" class="btn btn-light border text-black-50 shadow-none"><i class="fa fa-download"></i> Download</a> </div>
