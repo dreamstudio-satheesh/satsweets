b147c71d88e57a79fe20a20d8116e780e7dc1d0d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-02-12 19:15:08 +0530

    app

--- resources/views/livewire/invoice-list.blade.php
+++ resources/views/livewire/invoice-list.blade.php
@@ -66,10 +66,13 @@
                                                         <a href="javascript:void(0);" class="dropdown-item" data-bs-toggle="modal" wire:click="showpayment({{ $invoice->invoice_number }})" data-bs-target="#showpayment"><img src="{{ url('/assets/img/icons/dollar-square.svg') }}" class="me-2" alt="img">Show Payments</a>
                                                 </li>  
                                                 @endif
-                                                
+
+                                                @if ($invoice->status !='Paid')
                                                 <li>
                                                         <a href="javascript:void(0);" class="dropdown-item" data-bs-toggle="modal"  wire:click="createpayment({{ $invoice->id }})" data-bs-target="#createpayment"><img src="{{ url('') }}/assets/img/icons/plus-circle.svg" class="me-2" alt="img">Create Payment</a>
                                                 </li>
+                                                @endif
+                                                
                                                 <li>
                                                         <a href="javascript:void(0);" class="dropdown-item"><img src="{{ url('/assets/img/icons/download.svg') }}" class="me-2" alt="img">Download pdf</a>
                                                 </li>	
