71ba9a4a7e70fd5170ec33b850647e1258cb2619
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-02-16 03:49:05 +0530

    app

--- resources/views/livewire/invoice-list.blade.php
+++ resources/views/livewire/invoice-list.blade.php
@@ -104,7 +104,7 @@
         </div>
         
         <div class="card">
-            <div class="card-body" wire:ignore> 
+            <div class="card-body"> 
                 {{ $invoices->links('pagination') }}  
             </div>
             Total Records: {{ $invoices->total() }}   per page :{{ $invoices->perPage() }} 
