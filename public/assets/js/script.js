62abb064ea41c02118755b6f4d6a960d7921378d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-03-06 14:37:32 +0530

    app

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -120,7 +120,16 @@ $(document).ready(function(){
 			"sDom": 'fBtlpi',  
 			'pagingType': 'numbers', 
 			"ordering": true,
-			
+			"language": {
+				search: ' ',
+				sLengthMenu: '_MENU_',
+				searchPlaceholder: "Search...",
+				info: "_START_ - _END_ of _TOTAL_ items",
+			 },
+			initComplete: (settings, json)=>{
+				$('.dataTables_filter').appendTo('#tableSearch');
+				$('.dataTables_filter').appendTo('.search-input');
+			},	
 		});
 	}
 
